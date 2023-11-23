b5576d9af0ed9e682579092a208cd418b69d906c
Vlada <vlada_baranovao8@mail.ru>
2023-11-23 15:28:40 +0300

    open/close menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
- 
+document.addEventListener("DOMContentLoaded",  ()=> {
+
+    const menuButton = document.querySelector('.menu__button');
+    const closeMenuButton = document.querySelector('.close__button');
+    const menuPage = document.querySelector('.menu-page');
+    const startupPage = document.querySelector('.startup-page');
+
+    menuButton.addEventListener('click', () => {
+        menuPage.classList.remove('menu-page--invisible');
+        startupPage.classList.add('startup-page--invisible');
+    });
+
+    closeMenuButton.addEventListener('click', () => {
+        startupPage.classList.remove('startup-page--invisible');
+        menuPage.classList.add('menu-page--invisible');
+    });
+    
+});
+
+
+
+
