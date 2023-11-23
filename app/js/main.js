f3bce072540c9ab5951ecf2e89c531a8698c4e28
Vlada <vlada_baranovao8@mail.ru>
2023-11-23 18:26:37 +0300

    menu invisible fix

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded",  ()=> {
 
     const menuButton = document.querySelector('.menu__button');
     const closeMenuButton = document.querySelector('.close__button');
-    const menuPage = document.querySelector('.menu-page');
-    const startupPage = document.querySelector('.startup-page');
+    const menuPage = document.querySelector('.wrapper_mobile-menu');
+    const allPages = document.querySelector('.wrapper_mobile');
 
     menuButton.addEventListener('click', () => {
-        menuPage.classList.remove('menu-page--invisible');
-        startupPage.classList.add('startup-page--invisible');
+        menuPage.classList.remove('wrapper_mobile-menu--invisible');
+        allPages.classList.add('wrapper_mobile--invisible');
     });
 
     closeMenuButton.addEventListener('click', () => {
-        startupPage.classList.remove('startup-page--invisible');
-        menuPage.classList.add('menu-page--invisible');
+        allPages.classList.remove('wrapper_mobile--invisible');
+        menuPage.classList.add('wrapper_mobile-menu--invisible');
     });
     
 });
